a1592e227088c68769833f229ffbf60d61ef2193
BS <bs@bss-mac-pro.home>
2020-04-27 17:50:09 +0200

    modify content style

--- pages/index.js
+++ pages/index.js
@@ -27,9 +27,25 @@ const Homepage = ({ data }) => (
 				</span>
 				{data.sectionTwo.title}
 			</h1>
-			<div className="section">
-				<p className="section_content__para">{data.sectionTwo.text}</p>
-				<div section_cards></div>
+			<div className="">
+				<p className="para_width__50">{data.sectionTwo.text}</p>
+				<div className="section section_cards">
+					<article className="card">
+						<img src={data.sectionTwo.card1.icon} />
+						<p className="headingTwo">{data.sectionTwo.card1.title}</p>
+						<p>{data.sectionTwo.card1.text}</p>
+					</article>
+					<article className="card">
+						<img src={data.sectionTwo.card2.icon} />
+						<p className="headingTwo">{data.sectionTwo.card2.title}</p>
+						<p>{data.sectionTwo.card2.text}</p>
+					</article>
+					<article className="card">
+						<img src={data.sectionTwo.card3.icon} />
+						<p className="headingTwo">{data.sectionTwo.card3.title}</p>
+						<p>{data.sectionTwo.card3.text}</p>
+					</article>
+				</div>
 			</div>
 		</section>
 	</Wrapper>
