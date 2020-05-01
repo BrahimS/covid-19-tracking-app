660bcc0fa8f480e64fe1fc27182ee1a22e0056fc
BS <bs@bss-mac-pro.home>
2020-05-02 00:40:42 +0200

    add link to peronal site

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,10 @@
+import Link from "next/link";
 const Footer = () => (
-	<footer>{new Date().getFullYear()} © Brahim Sourny</footer>
+	<footer>
+		{new Date().getFullYear()}
+		<Link href="https://brahimsourny.pro">
+			<a className="">© Brahim Sourny</a>
+		</Link>
+	</footer>
 );
 export default Footer;
