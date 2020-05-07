7ed9c837f7951a5c6b32f75f9f1999e3f63b680f
BS <bs@bss-mac-pro.home>
2020-05-07 23:06:01 +0200

    remved the tested cases from the graph

--- components/Donut.js
+++ components/Donut.js
@@ -14,17 +14,12 @@ const Donut = ({ data: { updated, cases, recovered, deaths, tests } }) => {
 		<article className="donut_flex">
 			<Doughnut
 				data={{
-					labels: ["Tested", "Infected", "Recovered", "Deaths"],
+					labels: ["Infected", "Recovered", "Deaths"],
 					datasets: [
 						{
-							data: [tests, cases, recovered, deaths],
-							backgroundColor: ["#36A2EB", "#F3C052", "#81C5A3", "#ff0000"],
-							hoverBackgroundColor: [
-								"#36A2EB",
-								"#F3C052",
-								"#81C5A3",
-								"#ff0000",
-							],
+							data: [cases, recovered, deaths],
+							backgroundColor: ["#F3C052", "#81C5A3", "#ff0000"],
+							hoverBackgroundColor: ["#F3C052", "#81C5A3", "#ff0000"],
 							borderColor: "#000050",
 						},
 					],
