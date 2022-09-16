6be4b25fda22a6f4ff84fca22ed8db9f25545ef1
brahims <bsourny@gmail.com>
2022-09-16 16:08:50 +0200

    Add a option select input e2e Test

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -2,10 +2,8 @@ describe("Stats Page", () => {
 	it("should navigate to the Stats page", () => {
 		// Start from the index page
 		cy.visit("http://localhost:3000/");
-
 		// Find a link with an href attribute containing "Stats" and click it
 		cy.get("#stats").click();
-
 		// The new page should contain an h1 with "About page"
 		cy.get("form.picker_flex").contains("Choose a Country");
 	});
@@ -15,14 +13,20 @@ describe("About Page", () => {
 	it("should navigate to the about page", () => {
 		// Start from the index page
 		cy.visit("http://localhost:3000/");
-
 		// Find a link with an href attribute containing "about" and click it
 		cy.get('a[href*="about"]').click();
-
 		// The new url should include "/about"
 		cy.url().should("include", "/about");
-
 		// The new page should contain an h1 with "About page"
 		cy.get("h1").contains("Covid");
+	});
+});
+
+describe("Stats", () => {
+	it("Should navigate to Stats page, and slect the first conutry", () => {
+		cy.visit("http://localhost:3000/");
+		cy.get("#countrySelection")
+			.select("Choose a Country")
+			.should("have.text", "Choose a Country");
 	});
 });
