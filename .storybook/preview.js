9200ff2394e87bc60a453ccc953ffe290a0be109
brahims <bsourny@gmail.com>
2022-09-19 15:27:49 +0200

    COnfigure new storybook setup

---
+++ .storybook/preview.js
@@ -0,0 +1,11 @@
+import "/stories/header.css";
+import "/stories/page.css";
+export const parameters = {
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
